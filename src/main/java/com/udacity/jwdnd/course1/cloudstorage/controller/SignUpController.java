41722d50eb16549f27b4e460975a3b24db7be680
Victor Ochoa <ochoa.m.victor@gmail.com>
2021-03-11 18:56:43 -0800

    Added logic for login

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
@@ -4,11 +4,13 @@ import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/signup")
 public class SignUpController {
 
     private UserService userService;
@@ -17,7 +19,7 @@ public class SignUpController {
         this.userService = userService;
     }
 
-    @RequestMapping("/signup")
+    @GetMapping
     public String getSignUpPage() {
         return "signup";
     }
@@ -27,11 +29,11 @@ public class SignUpController {
         String signupError = null;
 
         if (!userService.isUsernameAvailable(user.getUsername())) {
-            signupError = "The username already exists.";
+            signupError = "The specified username is already in use. Please enter another username.";
         } else {
             int userId = userService.createUser(user);
             if (!userWasSuccessfullyCreated(userId))
-                signupError = "There was an error signing you up. Please try again.";
+                signupError = "Encountered an error when trying to sign up. Please try again.";
         }
 
         if (signupError == null) {
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.security;
 
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +11,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+    private EncryptionService encryptionService;
+
+    public SecurityConfig(AuthenticationService authenticationService, EncryptionService encryptionService){
+        this.authenticationService = authenticationService;
+        this.encryptionService = encryptionService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(this.authenticationService);
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
